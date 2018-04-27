6a082247d939068cefc4453554b21f7fedd6f0d6
VASILEIOS ZOIS <vzois001@ucr.edu>
2018-04-26 22:21:20 -0700

    taxi download script - extract columns

--- data/real/taxi/download.sh
+++ data/real/taxi/download.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-URL=https://s3.amazonaws.com/nyc-tlc/trip+data/yellow_tripdata_
+URL=https://s3.amazonaws.com/nyc-tlc/trip+data/
+PREFIX_FNAME=yellow_tripdata_
 START_YEAR=$1
 MONTH_COUNT=$2
+ACTION=$3
+
 DATE=$START_YEAR-01-01
+OUTPUT=all.csv
+rm -rf
 
 if (( $START_YEAR < 2009 ))
 then
@@ -16,8 +21,15 @@ do
 	NEXT_DATE=$(date +%Y-%m -d "$DATE + $n month")
 	#echo "$NEXT_DATE"
 	
-	FILE=$NEXT_DATE".csv"
-	echo "Downloading "$FILE" ... "
-	#wget -q --show-progress $URL$FILE
+	FILE=$PREFIX_FNAME$NEXT_DATE".csv"
+	
+	if [ $ACTION -eq 0 ]
+	then
+		echo "Downloading "$FILE" ... "
+		wget -q --show-progress $URL$FILE
+	fi
+	
+	echo "Extracting columns ... "
+	awk -F "\"*,\"*" '{print $5","$6}' $FILE > $OUTPUT
 
 done
